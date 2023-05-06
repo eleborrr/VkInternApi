4b1f396dbb7751e17806ecb0d807d99e92df4047
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-05-06 23:42:41 +0300

    User Controller draft methodes

---
+++ VkInternApi/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VkInternApi.Services.Auth.Attributes;
+
+namespace VkInternApi.Controllers;
+
+[Route("[controller]")]
+public class UserController: Controller
+{
+    [BasicAuthorization]
+    [HttpPost]
+    public IActionResult GetUserByIdAsync()
+    {
+        return Ok();
+    }
+    
+    [BasicAuthorization]
+    [HttpPost]
+    public IActionResult GetUsersAsync()
+    {
+        return Ok();
+    }
+    
+    [BasicAuthorization]
+    [HttpPost]
+    public IActionResult AddUserAsync()
+    {
+        return Ok();
+    }
+    
+    [BasicAuthorization]
+    [HttpPost]
+    public IActionResult DeleteUserByIdAsync()
+    {
+        return Ok();
+    }
+}
