3f2ecd6939e417a4c291946b3a823da095b47b07
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-05-06 22:00:51 +0300

    Initial commit

---
+++ VkInternApi/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace VkInternApi.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int UserGroupId { get; set; }
+    public int UserStateId { get; set; }
+}
