e1dca36dd294ea8984e3ed350d5b06387e973629
Gleb Petukhov <gleb-petuhov@mail.ru>
2023-05-09 20:45:51 +0300

    postgresql connection added

--- VkInternApi/Data/ApplicationDbContext.cs
+++ VkInternApi/Data/ApplicationDbContext.cs
@@ -7,7 +7,12 @@ public class ApplicationDbContext: DbContext
 {
     public ApplicationDbContext() : base()
     {}
-    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=VkInternDB;Username=postgres;Password=18ZIfigi_");
+    }
+
     public DbSet<User> Users { get; set; }
     public DbSet<UserGroup> UserGroups {get; set; }
     public DbSet<UserState> UserStates { get; set; }
